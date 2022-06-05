7649ca21fa369166a926608c389d7e4abccdbaf9
jhawkesworth <jhawkesworth@users.noreply.github.com>
2022-06-05 09:26:19 +0100

    add super-basic c to f and f to c converter

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,102 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket_dyn_templates::{Template, context};
+
+
+// use rocket_dyn_templates::{Template, context};
 use rocket::{Build, Rocket};
+use rocket::response::status;
+use rocket::request::FromParam;
+
+struct IntTemp {
+    degrees: i32
+}
+
+impl<'a> FromParam<'a> for IntTemp<> {
+    type Error = &'a str;
+
+    fn from_param(param: &'a str) -> Result<Self, Self::Error> {
+        param.chars().all(|c| c.is_alphanumeric() || c.eq_ignore_ascii_case(&'-'))
+            .then(|| IntTemp{degrees: param.parse::<i32>().unwrap()})
+            .ok_or(param)
+    }
+}
 
 #[catch(404)] fn not_found() -> &'static str { "Nothing here, sorry!" }
-// #[catch(500)] fn just_500() -> &'static str { "Whoops!?" }
+#[catch(500)] fn just_500() -> &'static str { "Whoops!?" }
 // #[catch(default)] fn some_default() -> &'static str { "Everything else." }
 
-#[get("/hello")]
-fn hello() -> Template {
-    Template::render("hello", context! { greeting: "Hello, world!  I am a rocket shuttle app", })
-}
-//
 // #[get("/hello")]
-// fn hello() -> &'static str {
-//     "Hello, world!"
+// fn hello() -> Template {
+//     Template::render("hello", context! { greeting: "Hello, world!  I am a rocket shuttle app", })
 // }
-
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world! I am an index page"
-}
 
 // #[get("/")]
 // fn index() -> Template {
 //     Template::render("index", context! { greeting: "Hello, world!  I am an index page", })
 // }
 
+#[get("/hello")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world! I am an index page"
+}
+
+#[get("/ctof/<celsius>")]
+fn c_to_f(celsius: IntTemp) -> Option<String> {
+    let converted = (celsius.degrees * 9 / 5 ) +32;
+    Option::from(converted.to_string())
+}
+
+#[get("/ftoc/<farenheit>")]
+fn f_to_c(farenheit: IntTemp) -> Option<String> {
+    let converted = (farenheit.degrees - 32) * 5 / 9;
+    Option::from(converted.to_string())
+}
+
+
+
 #[shuttle_service::main]
 async fn rocket() -> Result<Rocket<Build>, shuttle_service::Error> {
     let rocket = rocket::build()
-        // .register("/duff", catchers![just_500, some_default])
-        //.register("/foo", catchers![not_found])
-        .mount("/", routes![index])
-        .mount("/api", routes![hello])
-        .attach(Template::fairing())
+        .register("/duff", catchers![just_500])
+        .register("/", catchers![not_found, just_500])
+        .mount("/", routes![index, hello, c_to_f, f_to_c])
+        // as of 0.3.0 shuttle just does not support anything external to the compiled .so
+        // so we can't use rocket's dynamic template support at the moment.
+        //.mount("/templates", routes![hello])
+        //.attach(Template::fairing())
         ;
 
     Ok(rocket)
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn c_to_f_works() {
+        // 10 C = 50 F.
+        let result = c_to_f(IntTemp { degrees: 10 });
+        assert_eq!(result, Option::Some(String::from("50")));
+    }
+
+    #[test]
+    fn f_to_c_works() {
+        // 50 F = 10 C.  ish
+        let result = f_to_c(IntTemp { degrees: 50 });
+        assert_eq!(result, Option::Some(String::from("10")));
+    }
 }
